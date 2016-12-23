d51e588d7490a4a099f483329dfd6af3de587f8b
Wojciech Kozlowski <wojciech.kozlowski@vivaldi.net>
2016-12-23 14:49:36 +0000

    Moved my patches onto config.def.h to make rebasing easier

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Liberation Mono:pixelsize=15:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -85,30 +85,32 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	"#000000", // "black",
+	"#b21818", // "red3",
+	"#18b218", // "green3",
+	"#b26818", // "yellow3",
+	"#1818b2", // "blue2",
+	"#b218b2", // "magenta3",
+	"#18b2b2", // "cyan3",
+	"#b2b2b2", // "gray90",
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	"#686868", // "gray50",
+	"#ff5454", // "red",
+	"#54ff54", // "green",
+	"#ffff54", // "yellow",
+	"#5454ff", // "#5c5cff",
+	"#ff54ff", // "magenta",
+	"#54ffff", // "cyan",
+	"#ffffff", // "white",
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#cccccc",
 	"#555555",
+	"#93a1a1",
+	"#002b36",
 };
 
 
@@ -116,8 +118,8 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
+unsigned int defaultfg = 258;
+unsigned int defaultbg = 259;
 static unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
 
